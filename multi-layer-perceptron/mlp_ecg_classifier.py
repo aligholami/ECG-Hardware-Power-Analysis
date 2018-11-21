2263338a6f6c94a863832190d742376dd44e84ca
Ali Gholami <aligholamee@aut.ac.ir>
2018-11-22 00:15:11 +0330

    Update: Complete class names

--- multi-layer-perceptron/mlp_ecg_classifier.py
+++ multi-layer-perceptron/mlp_ecg_classifier.py
@@ -12,8 +12,9 @@ import keras_metrics
 from keras.utils import plot_model
 from keras import regularizers
 from sklearn.metrics import classification_report
+from confusion_matrix import plot_confusion_matrix_from_data
 
-class_names = ['N', 'S', 'V', 'F', 'Q']
+class_names = ['Normal', 'Supraventricular ectopic beat', 'Ventricular ectopic beat', 'Fusion beat', 'Unknown beat']
 num_classes = 5
 num_epochs = 20
 batch_size = 128
@@ -36,11 +37,12 @@ model.compile(loss='categorical_crossentropy',
               optimizer=keras.optimizers.Adam(lr=1e-4, beta_1=0.9, beta_2=0.999),
               metrics=['accuracy', precision, recall])
 
-model.fit(x=x_train, y=y_train, epochs=num_epochs, batch_size=batch_size, shuffle=True, class_weight={0: 0.1, 1: 0.2, 2: 0.2, 3: 0.2, 4: 0.3})
+# model.fit(x=x_train, y=y_train, epochs=num_epochs, batch_size=batch_size, shuffle=True, class_weight={0: 0.1, 1: 0.2, 2: 0.2, 3: 0.2, 4: 0.3})
 # plot_model(model, to_file='vis.png')
 
 y_pred = model.predict_classes(x_test)
 y_test = np.argmax(y_test, axis=1) # Convert one-hot to index
+plot_confusion_matrix_from_data(y_test, y_pred, columns=class_names)
 print(classification_report(y_test, y_pred))
 
 # score = model.evaluate(x_test, y_test)
