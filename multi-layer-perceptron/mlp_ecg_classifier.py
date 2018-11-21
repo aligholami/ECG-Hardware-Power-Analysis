9b8be4742ba02c98f128225e64130d66dac7422f
Ali Gholami <aligholamee@aut.ac.ir>
2018-11-21 18:34:48 +0330

    Update: Fix loss and accuracy for evaluation

--- multi-layer-perceptron/mlp_ecg_classifier.py
+++ multi-layer-perceptron/mlp_ecg_classifier.py
@@ -8,13 +8,14 @@ num_classes = 5
 num_epochs = 20
 batch_size = 128
 data_root = './data/'
+input_shape = (188, 1)
 
 (x_train, y_train), (x_test, y_test) = load_data(data_root)
 
 model = Sequential()
 
-model.add(Dense(units=128, activation='relu', input_dim=_))
-model.add(Dense(units=64, activation='relu', input_dim=_))
+model.add(Dense(units=128, activation='relu', input_shape=input_shape))
+model.add(Dense(units=64, activation='relu'))
 model.add(Dense(units=num_classes, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy',
@@ -23,4 +24,6 @@ model.compile(loss='categorical_crossentropy',
 
 model.fit(x=x_train, y=y_train, epochs=num_epochs, batch_size=batch_size)
 
-loss_metrics = model.evaluate(x=x_test, y=y_test, batch_size=batch_size)
+score = model.evaluate(x_test, y_test)
+print("Test loss: ", score[0])
+print("Test accuracy: ", score[1])
