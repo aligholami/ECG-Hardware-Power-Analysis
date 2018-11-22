71a5c642a395bfe11ebf9da07100434ecff67140
Ali Gholami <ali.gholami@sharif.edu>
2018-11-23 01:36:13 +0330

    Fix repo

---
+++ tools/augmenter.py
@@ -0,0 +1,36 @@
+import numpy as np
+from scipy.signal import resample
+import random
+import config
+
+def stretch(x):
+    l = int(187 * (1 + (random.random()-0.5)/3))
+    y = resample(x, l)
+    if l < 187:
+        y_ = np.zeros(shape=(187, ))
+        y_[:l] = y
+    else:
+        y_ = y[:187]
+    return y_
+
+def amplify(x):
+    alpha = (random.random()-0.5)
+    factor = -alpha*x + (1+alpha)
+    return x*factor
+
+def augment_by_n(x):
+    augmented = np.zeros(shape=(config.AUGMENT_N_TIMES, config.NUM_FEATURES - 1))
+
+    for i in range(config.AUGMENT_N_TIMES - 1):
+        if random.random() < 0.33:
+            new_y = stretch(x)
+
+        elif random.random() < 0.66:
+            new_y = amplify(x)
+
+        else:
+            new_y = stretch(x)
+            new_y = amplify(x)
+    
+        augmented[i, :] = new_y
+    return augmented
---
+++ tools/config.py
@@ -0,0 +1,6 @@
+
+TRAIN_FILE_NAME = 'mitbih_train.csv'
+TEST_FILE_NAME = 'mitbih_test.csv'
+
+AUGMENT_N_TIMES = 20
+NUM_FEATURES = 188
