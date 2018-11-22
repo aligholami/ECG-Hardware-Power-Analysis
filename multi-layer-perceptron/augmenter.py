a997c9e1c9bc328585f48e06ef6a5df5cb07ffbe
Ali Gholami <aligholamee@aut.ac.ir>
2018-11-22 21:56:55 +0330

    Update: Added strecth and augmentation

--- multi-layer-perceptron/augmenter.py
+++ multi-layer-perceptron/augmenter.py
@@ -1,6 +1,22 @@
 import numpy as np
+from scipy.signal import resample
 import random
 import config
+
+def stretch(x):
+    l = int(187 * (1 + (random.random()-0.5)/3))
+    y = resample(x, l)
+    if l < 187:
+        y_ = np.zeros(shape=(187, ))
+        y_[:l] = y
+    else:
+        y_ = y[:187]
+    return y_
+
+def amplify(x):
+    alpha = (random.random()-0.5)
+    factor = -alpha*x + (1+alpha)
+    return x*factor
 
 def augment_by_n(x):
     augmented = np.zeros(shape=(config.AUGMENT_N_TIMES, config.NUM_FEATURES))
