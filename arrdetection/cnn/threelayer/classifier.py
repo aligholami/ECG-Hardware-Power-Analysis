8699f9bf131ee1b9922da23fe5cf1eb49457abf3
Ali Gholami <aligholamee@aut.ac.ir>
2018-11-23 15:03:21 +0330

    Update: Added CNN model with Skip connections

---
+++ arrdetection/cnn/threelayer/classifier.py
@@ -0,0 +1,102 @@
+import sys
+sys.path.insert(0, '../../tools/')
+
+import tensorflow as tf
+import keras
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.models import Model
+from keras.layers import Input, Dense, Conv1D, MaxPooling1D, Softmax, Add, Flatten, Activation
+from keras.utils import to_categorical
+from utils import load_data, as_keras_metric, precision, recall, fmeasure
+import keras_metrics
+from keras.utils import plot_model
+from keras import regularizers
+from sklearn.metrics import classification_report
+from confusion_matrix import plot_confusion_matrix_from_data
+
+class_names = ['Normal', 'Supraventricular ectopic beat', 'Ventricular ectopic beat', 'Fusion beat', 'Unknown beat']
+num_classes = 5
+num_epochs = 100
+batch_size = 256
+data_root = '../../data/'
+num_features = (187)
+
+(x_train, y_train), (x_test, y_test) = load_data(data_root)
+
+print("Train shapes:", x_train.shape, y_train.shape)
+print("Test shapes:", x_test.shape, y_test.shape)
+
+# convert class vectors to binary class matrices
+y_train = to_categorical(y_train, num_classes)
+y_test = to_categorical(y_test, num_classes)
+
+inp = Input(shape=(num_features, 1))
+C = Conv1D(filters=32, kernel_size=5, strides=1)(inp)
+
+C11 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(C)
+A11 = Activation("relu")(C11)
+C12 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(A11)
+S11 = Add()([C12, C])
+A12 = Activation("relu")(S11)
+M11 = MaxPooling1D(pool_size=5, strides=2)(A12)
+
+
+C21 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(M11)
+A21 = Activation("relu")(C21)
+C22 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(A21)
+S21 = Add()([C22, M11])
+A22 = Activation("relu")(S11)
+M21 = MaxPooling1D(pool_size=5, strides=2)(A22)
+
+
+C31 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(M21)
+A31 = Activation("relu")(C31)
+C32 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(A31)
+S31 = Add()([C32, M21])
+A32 = Activation("relu")(S31)
+M31 = MaxPooling1D(pool_size=5, strides=2)(A32)
+
+
+C41 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(M31)
+A41 = Activation("relu")(C41)
+C42 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(A41)
+S41 = Add()([C42, M31])
+A42 = Activation("relu")(S41)
+M41 = MaxPooling1D(pool_size=5, strides=2)(A42)
+
+
+C51 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(M41)
+A51 = Activation("relu")(C51)
+C52 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(A51)
+S51 = Add()([C52, M41])
+A52 = Activation("relu")(S51)
+M51 = MaxPooling1D(pool_size=5, strides=2)(A52)
+
+F1 = Flatten()(M51)
+
+D1 = Dense(32)(F1)
+A6 = Activation("relu")(D1)
+D2 = Dense(32)(A6)
+D3 = Dense(5)(D2)
+A7 = Softmax()(D3)
+
+model = Model(inputs=inp, outputs=A7)
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=keras.optimizers.Adam(lr=1e-4, beta_1=0.9, beta_2=0.999),
+              metrics=['accuracy', precision, recall])
+
+model.fit(x=x_train, y=y_train, epochs=num_epochs, batch_size=batch_size, shuffle=True)
+
+
+y_pred = model.predict(x_test)
+y_pred = np.argmax(y_pred, axis=1)
+print(y_pred)
+y_test = np.argmax(y_test, axis=1) # Convert one-hot to index
+plot_confusion_matrix_from_data(y_test, y_pred, columns=class_names)
+print(classification_report(y_test, y_pred))
